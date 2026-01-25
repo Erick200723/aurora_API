fc00ca5be6a6c39f402a4d7ccf081a0505b45b10
Erick200723 <erick2007gabriel23@gmail.com>
2026-01-24 22:27:04 -0300

    ajuste na porta pra enviar codigo de verificação

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -3,11 +3,14 @@ import nodemailer from "nodemailer"
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: Number(process.env.MAIL_PORT),
-  secure: false, 
+  secure: Number(process.env.MAIL_PORT) === 465, 
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 })
 
 export async function sendOTPEmail(email: string, code: string) {
